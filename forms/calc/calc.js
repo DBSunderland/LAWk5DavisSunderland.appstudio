ff9a54dae63021a558c5b1f1c97f4fc1c76f133b
DBSunderland <davissunderland@creighton.edu>
2021-03-03 15:05:05 -0600

    15:05 update

---
+++ forms/calc/calc.js
@@ -0,0 +1,13 @@
+/*
+number1 = parseInt(prompt('Enter the first number'))
+number2 = parseInt(prompt('Enter the second number'))
+
+function calculate (num1, num2) {
+  sum = num1+num2
+  return sum
+  }
+  
+let yourSum = calculate(number1, number2)
+
+console.log(`The sum of ${number1} and ${number2} is ${yourSum}.`)
+*/
